f926e77b7c3f751fb361243dcb6fa00a967232c3
Daws <castrogiraldosantiago@gmail.com>
2024-11-22 09:32:18 -0500

    Feat: MAchin and REntal DAO creation

---
+++ src/main/java/com/printadmin/persistance/DAO/MachineDAO.java
@@ -0,0 +1,9 @@
+package com.printadmin.persistance.DAO;
+
+public class MachineDAO {
+//TODOCreate Machines
+//TODOget all machines paginated(5 machines per page)
+//TODOupdate state to true/false(avaliable/rented)
+//TODOimport info from a excel
+//TODOchecks if a serial alredy exist 
+}
---
+++ src/main/java/com/printadmin/persistance/DAO/RentalDAO.java
@@ -0,0 +1,8 @@
+package com.printadmin.persistance.DAO;
+
+public class RentalDAO {
+    //TODOcreate rental
+    //TODOupdate state to true/false(active/disable)
+
+
+}
