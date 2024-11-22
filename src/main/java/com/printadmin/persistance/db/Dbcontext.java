f2d304807e1a033697f93c6c72ec0fb1ecfb1ca7
Daws <castrogiraldosantiago@gmail.com>
2024-11-22 11:56:50 -0500

    fix: db conexction

--- src/main/java/com/printadmin/persistance/db/Dbcontext.java
+++ src/main/java/com/printadmin/persistance/db/Dbcontext.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class Dbcontext {
     //jdbc:mysql://HOST:PORT/DATABASE_NAME
-    private static final String URL="bvnqfza1h7mjqp1cboja-mysql.services.clever-cloud.com:3306/bvnqfza1h7mjqp1cboja";
+    private static final String URL="jdbc:mysql://bvnqfza1h7mjqp1cboja-mysql.services.clever-cloud.com:3306/bvnqfza1h7mjqp1cboja";
     private static final String USER="ucp1werio5sstnfj";
     private static final String PASSWORD="oZkQQoAp2AcpDGHz19VN";
     
