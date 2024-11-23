bf7271dd0a035f35c1af754783f338f7cae8d09b
Daws <castrogiraldosantiago@gmail.com>
2024-11-23 16:51:14 -0500

    Feat: Client controller definicion

--- src/main/java/com/printadmin/controllers/ClinetController.java
+++ src/main/java/com/printadmin/controllers/ClinetController.java
@@ -1,5 +1,8 @@
 package com.printadmin.controllers;
 
+import java.util.List;
+
+import com.printadmin.entities.Client;
 import com.printadmin.persistance.DAO.ClientDAO;
 
 public class ClinetController {
@@ -7,7 +10,24 @@ public class ClinetController {
     public ClinetController(){
         ClientDAO= new ClientDAO();
     }
-    //TODOcreate Client
+    //create Client
+    public String CreateClient(String name, String last_name, String email, String dir){
+        Client NewClient= new Client(name, last_name, email, dir);
+        if (!NewClient.CheckEmpty()) {
+            
+            this.ClientDAO.addClient(NewClient);
+                return "Cliente agregado con exito";
+        }
+
+        return "Imposible crear cliente, campos incompletos";
+        
+
+    }
     
-    //TODO See all clients
+    //See all clients
+    public List<Client> ShowClients(){
+        if(this.ClientDAO.GetAllClients().isEmpty()){ return  null; }
+
+        return this.ClientDAO.GetAllClients();
+    }
 }
