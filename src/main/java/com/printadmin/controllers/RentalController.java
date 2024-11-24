9cf159f7ce1a7931da4d4b915047d703d2fb40c9
Daws <castrogiraldosantiago@gmail.com>
2024-11-23 19:22:40 -0500

    Rental controller  definition of 2/3 methos

--- src/main/java/com/printadmin/controllers/RentalController.java
+++ src/main/java/com/printadmin/controllers/RentalController.java
@@ -1,5 +1,9 @@
 package com.printadmin.controllers;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import com.printadmin.entities.Rental;
 import com.printadmin.persistance.DAO.RentalDAO;
 
 public class RentalController {
@@ -7,10 +11,24 @@ public class RentalController {
     private RentalDAO RentalDAO;
 
     public RentalController() {
-        this.RentalDAO = new RentalDAO();
+        RentalDAO = new  RentalDAO();
     }
     //TODOadd machines
+    public void CreateRental(String starDate, String endDate, boolean state, int client_id, int machine_id){
+        LocalDate StarDate= LocalDate.parse(starDate);
+        LocalDate EndDate= LocalDate.parse(endDate);
+        Rental rent= new Rental(StarDate, EndDate, client_id, machine_id);
+        this.RentalDAO.addRental(rent);
+
+    }
     //TODOshow all the rentasl only active / inactive
+    public List<Rental> Show(boolean option){
+        if (this.RentalDAO.getRentals(false).isEmpty()) {return null;}
+        return this.RentalDAO.getRentals(option);
+
+
+    }
+
     //TODOupdate state to true/false(active/disable)
 
 
